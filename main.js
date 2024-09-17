7b89e4c3e1511cd0a3f1a74c1742f6ef94c4dcad
JesusOrmaza <jesus@admins-MacBook-Pro.local>
2024-09-17 01:18:41 +0100

    Fixing mobile version

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const button = document.getElementById("open-button");
 button.addEventListener('click', () => { // add a click event listener
     if (document.body.classList.contains('show-menu'))
+    {
         document.querySelector('body').classList.remove('show-menu');
+        document.getElementById('menu').style.display = 'none';
+    }
     else
+    {
         document.querySelector('body').classList.add('show-menu');
+        document.getElementById('menu').style.display = 'block';
+    }
+        
   });
+
+function onClickMenuItem() {
+    document.querySelector('body').classList.remove('show-menu');
+    document.getElementById('menu').style.display = 'none';
+}
